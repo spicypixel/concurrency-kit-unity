6b1d90307567c455f947cb82eec786f033e5da29
Aaron Oneal <aaron.oneal@spicypixel.com>
2017-08-26 21:12:54 -0700

    Update dependencies and include source

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,153 +1,35 @@
 import * as gulp from "gulp";
-import * as gutil from "gulp-util";
-import * as path from "path";
-
 import * as BuildKit from "@spicypixel/build-kit-js";
 import * as CoreKit from "@spicypixel/core-kit-js";
 import * as UnityKit from "@spicypixel/unity-kit-js";
-import Promise = CoreKit.Promise;
 
-const moduleName: string = "ConcurrencyKit";
+const unityModule = UnityKit.UnityModule.createFromPath(
+  __dirname, "SpicyPixel", "ConcurrencyKit");
+const libraryReference = new UnityKit.UnityModuleLibraryReference(
+  unityModule, new BuildKit.NodeModule("@spicypixel/concurrency-kit-cs"));
 
-// Default task to run continuous integration
-gulp.task("default", ["package"]);
+gulp.task("default", () => defaultAsync());
+gulp.task("clean", () => cleanAsync());
+gulp.task("package", () => packageAsync());
+gulp.task("install", () => installAsync());
 
-// Install
-gulp.task("install", () =>
-  cleanDependencies()
-    .then(() => copyDependenciesToAssets())
-    .then(() => installAsModule()));
-
-gulp.task("package", () =>
-  cleanDependencies()
-    .then(() => copyDependenciesToAssets())
-    .then(() => buildUnityPackage()));
-
-const unityProject = new UnityKit.UnityProject(__dirname);
-const assetsDir = path.join(__dirname, "Assets");
-const kitDir = path.join(assetsDir, "SpicyPixel", "Modules", moduleName);
-
-async function cleanDependencies(): Promise<void> {
-  gutil.log(gutil.colors.cyan("Cleaning ..."));
-
-  await CoreKit.FileSystem.removePatternsAsync("Artifacts");
-  await CoreKit.FileSystem.removePatternsAsync([
-    "Bin",
-    "Docs",
-    "MonoDoc/SpicyPixel." + moduleName + ".*"
-  ], { globOptions: { cwd: kitDir } });
+async function defaultAsync() {
+  await packageAsync();
 }
 
-function copyDependenciesToAssets(): Promise<Promise<void>[]> {
-  const nodeModuleDir = require.resolve("@spicypixel/concurrency-kit-cs")
-    .match(/.*\/node_modules\/@spicypixel\/[^/]+\//)[0];
-
-  const docsSrcDir = path.join(nodeModuleDir, "Docs");
-  const sourceSrcDir = path.join(nodeModuleDir, "Source");
-  const docsDestDir = path.join(kitDir, "Docs");
-  const monoDocDestDir = path.join(kitDir, "MonoDoc");
-  const binDestDir = path.join(kitDir, "Bin");
-  const testDestDir = path.join(binDestDir, "Editor");
-
-  const binAssemblies = [
-    "System.Threading",
-    "SpicyPixel.Threading",
-    "SpicyPixel.Threading.Unity"];
-
-  const testAssemblies = [
-    "SpicyPixel.Threading.Test",
-    "SpicyPixel.Threading.Unity.Test"];
-
-  const monoDocs = [
-    "SpicyPixel." + moduleName + ".source",
-    "assemble/SpicyPixel." + moduleName + ".tree",
-    "assemble/SpicyPixel." + moduleName + ".zip",
-  ];
-
-  let promises: Promise<void>[] = [];
-
-  binAssemblies.forEach(assembly => {
-    const srcDir = path.join(sourceSrcDir, assembly, "bin", "Release");
-    promises = promises.concat(
-      CoreKit.FileSystem.copyPatternsAsync(
-        path.join(srcDir, assembly + ".dll"),
-        binDestDir,
-        { base: srcDir }
-      ));
-  });
-
-  testAssemblies.forEach(assembly => {
-    const srcDir = path.join(sourceSrcDir, assembly, "bin", "Release");
-    promises = promises.concat(
-      CoreKit.FileSystem.copyPatternsAsync(
-        path.join(srcDir, assembly + ".dll"),
-        testDestDir,
-        { base: srcDir }
-      ));
-  });
-
-  promises = promises.concat(
-    CoreKit.FileSystem.copyPatternsAsync(
-      path.join(docsSrcDir, "**/*"),
-      docsDestDir,
-      { base: docsSrcDir }
-    ));
-
-  promises = promises.concat(
-    CoreKit.FileSystem.copyPatternsAsync(
-      monoDocs,
-      monoDocDestDir,
-      { cwd: path.join(nodeModuleDir, "MonoDoc"), flatten: true }
-    ));
-
-  promises = promises.concat(
-    CoreKit.FileSystem.copyPatternsAsync(
-      ["README.md", "LICENSE.md"],
-      kitDir,
-      { cwd: nodeModuleDir }
-    ));
-
-  return Promise.all(promises);
+async function cleanAsync() {
+  await unityModule.cleanArtifactsAsync();
+  await libraryReference.uninstallAsync();
 }
 
-async function installAsModule(): Promise<void> {
-  // Install as a module into a Unity Assets folder if it exists.
-  // CWD path is relative to a node_modules/<this scope>/<this module> install.
-  const parentAssetsDir = path.join(__dirname, "..", "..", "..", "Assets");
-
-  try {
-    await CoreKit.FileSystem.Directory.accessAsync(parentAssetsDir,
-      CoreKit.FileSystem.FileSystemPermission.Visible);
-  }
-  catch (err) {
-    gutil.log("Skipping asset install because folder does not exist: ", parentAssetsDir);
-    return;
-  }
-
-  gutil.log("Proceeding with asset install");
-
-  const srcDir = path.join(__dirname, "Assets", "SpicyPixel", "Modules", moduleName);
-  const destDir = path.join(parentAssetsDir, "SpicyPixel", "Modules", moduleName);
-
-  await CoreKit.FileSystem.copyPatternsAsync(
-    [path.join(srcDir, "**/*"), "!**/*.meta"],
-    destDir,
-    { base: srcDir }
-  );
+async function installAsync() {
+  await libraryReference.installAsync(
+    [], [],
+    ["System.Threading", "SpicyPixel.Threading", "SpicyPixel.Threading.Unity"],
+    ["SpicyPixel.Threading.Test", "SpicyPixel.Threading.Unity.Test"]);
 }
 
-async function buildUnityPackage(): Promise<void> {
-  try {
-    await CoreKit.FileSystem.Directory.accessAsync(assetsDir,
-      CoreKit.FileSystem.FileSystemPermission.Visible);
-  }
-  catch (err) {
-    gutil.log("Skipping build Unity package because Bin folder does not exist: ", assetsDir);
-    return;
-  }
-
-  await CoreKit.FileSystem.Directory.createRecursiveAsync("Artifacts");
-  const tag = await BuildKit.GitRevision.tagAsync();
-  await unityProject.packageAsync(["Assets/SpicyPixel/Modules/" + moduleName],
-    "./Artifacts/SpicyPixel." + moduleName + "-" + tag + ".unitypackage");
+async function packageAsync() {
+  await installAsync();
+  await unityModule.exportPackageAsync();
 }
--- typings/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="globals/gulp-util/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="modules/gulp/index.d.ts" />
